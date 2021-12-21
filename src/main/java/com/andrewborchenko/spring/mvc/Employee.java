4dde0e30436e9ea6f774e7f611271912d24e83ee
andrew <andrei-borchenko@mail.ru>
2021-12-21 18:52:35 +0300

    mvc2

--- src/main/java/com/andrewborchenko/spring/mvc/Employee.java
+++ src/main/java/com/andrewborchenko/spring/mvc/Employee.java
@@ -1,11 +1,55 @@
 package com.andrewborchenko.spring.mvc;
 
+import com.andrewborchenko.spring.mvc.validation.CheckEmail;
 import lombok.Data;
+
+import javax.validation.constraints.*;
+import java.util.HashMap;
+import java.util.Map;
 
 @Data
 public class Employee {
+    @Size(min = 6, max = 15, message = "name must be between 6 and 15 symbols")
     private String name;
+    //Проверка что поле не из одних пробелов
+    @NotBlank(message = "this field is required")
     private String surname;
+    @Min(value = 1, message = "salary must be more than zero")
     private int salary;
     private String department;
+    private Map<String, String> departments;
+    private String carBrand;
+    private Map<String, String> carBrands;
+    private String[] languages;
+    @Pattern(regexp = "\\d{3}-\\d{3}-\\d{4}", message = "Please use this pattern XXX-XXX-XXXX")
+    private String phoneNumber;
+    @CheckEmail
+    private String email;
+
+    public Employee() {
+        departments = new HashMap<>();
+        departments.put("IT", "Information Technology" );
+        departments.put("BIO", "Biology" );
+        departments.put("Math", "Mathematics" );
+
+        carBrands = new HashMap<>();
+        carBrands.put("BMW", "BMW");
+        carBrands.put("Audi", "Audi");
+        carBrands.put("Kia", "Kia");
+
+        languages = new String[3];
+        languages[0] = "English";
+        languages[1] = "French";
+        languages[2] = "Spanish";
+    }
+/*
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", salary=" + salary +
+                ", department='" + department + '\'' +
+                '}';
+    }*/
 }
--- src/main/webapp/WEB-INF/view/show-emp-details-view.jsp
+++ src/main/webapp/WEB-INF/view/show-emp-details-view.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%--
   Created by IntelliJ IDEA.
   User: andrew
@@ -5,7 +6,7 @@
   Time: 16:05
   To change this template use File | Settings | File Templates.
 --%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page contentType="text/html;charset=UTF-8"%>
 <html>
 <body>
 <h2>Dear employee you are welcome</h2>
@@ -14,5 +15,21 @@
 <%--Your name is ${param.employeeName}--%>
 
 Your name is ${employee.toString()}
+
+<%--Unordered list--%>
+<%--для вывода нескольких значений выбираем forEach(jstl/.../core)--%>
+<ul>
+    <c:forEach var="lang" items="${employee.languages}">
+        <%--li - listItem(элемент списка)--%>
+        <li>
+         ${lang}
+        </li>
+    </c:forEach>
+</ul>
+
+Your phone number is: ${employee.phoneNumber}
+
+Your email is: ${employee.email}
+
 </body>
 </html>
